60a59ffc1f0120870b3e097f1388400c254bc212
Braden <108199884+bradenr402@users.noreply.github.com>
2024-06-13 20:52:36 -0400

    add `days_ago` helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,19 @@ module ApplicationHelper
       "#{minutes}:#{'%02d' % seconds}"
     end
   end
+
+  def days_ago(date)
+    # Calculate the difference in days between the given date and today
+    days_difference = (Date.today - date.to_date).to_i
+
+    # Return the appropriate string based on the difference
+    case days_difference
+    when 0
+      'today'
+    when 1
+      'yesterday'
+    else
+      "#{days_difference} days ago"
+    end
+  end
 end
