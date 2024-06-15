0034f6f852f23abe850272be9500e712d1d42591
Braden <108199884+bradenr402@users.noreply.github.com>
2024-06-15 14:34:38 -0400

    fix: pluralization and move to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,40 @@ module ApplicationHelper
       "#{days_difference} days ago"
     end
   end
+
+  def precise_distance_of_time_in_words(from_time, to_time = 0, options = {})
+    options = { include_seconds: true, highest_measure_only: false }.merge(
+      options
+    )
+
+    from_time = from_time.to_time if from_time.respond_to?(:to_time)
+    to_time = to_time.to_time if to_time.respond_to?(:to_time)
+
+    distance = (to_time - from_time).abs
+    parts = calculate_time_parts(distance, options)
+
+    if parts.empty?
+      options[:include_seconds] ? '0 seconds' : '0 minutes'
+    else
+      parts.join(', ')
+    end
+  end
+
+  private
+
+  def calculate_time_parts(distance, options)
+    parts = []
+
+    %w[year month week day hour minute second].each do |interval|
+      break if distance.zero? && options[:highest_measure_only]
+
+      next unless distance >= 1.send(interval)
+
+      number = (distance / 1.send(interval)).floor
+      distance -= number.send(interval)
+      parts << pluralize(number, interval)
+    end
+
+    parts
+  end
 end
