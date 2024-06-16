744a5f29f95ee31a63e29c4416eb5f4d3aa06081
Braden <108199884+bradenr402@users.noreply.github.com>
2024-06-15 21:12:18 -0400

    users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[show]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
