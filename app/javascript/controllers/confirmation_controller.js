9443859cdd7940852bd39e34cf68aa9f2f0fbe98
Braden <108199884+bradenr402@users.noreply.github.com>
2024-06-21 14:10:47 -0400

    remove 'connected' message

--- app/javascript/controllers/confirmation_controller.js
+++ app/javascript/controllers/confirmation_controller.js
@@ -4,10 +4,6 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static values = { message: String };
 
-  connect() {
-    console.log('conneced')
-  }
-
   confirm(event) {
     if (!window.confirm(this.messageValue)) {
       event.preventDefault();
