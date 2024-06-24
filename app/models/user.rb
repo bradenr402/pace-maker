359a23d5f4ced53fe72c7840884b8cc76de28c63
Braden <108199884+bradenr402@users.noreply.github.com>
2024-06-23 22:22:35 -0400

    add dependent: :destroy to owned_teams

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :runs, dependent: :destroy
   has_many :team_memberships, dependent: :destroy
   has_many :teams, through: :team_memberships
-  has_many :owned_teams, class_name: 'Team', foreign_key: 'owner_id'
+  has_many :owned_teams, class_name: 'Team', foreign_key: 'owner_id', dependent: :destroy
   has_many :team_join_requests
 
   # Include default devise modules. Others available are:
