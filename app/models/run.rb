224ab44c5fe88cee33f02d23becaf4583648c509
bradenr402 <bradenr402@gmail.com>
2024-07-08 19:26:47 -0400

    use `.all_day` for runs today scope

--- app/models/run.rb
+++ app/models/run.rb
@@ -14,7 +14,7 @@ class Run < ApplicationRecord
   attr_accessor :duration_input
 
   scope :in_date_range, ->(range) { where(date: range).order(date: :desc) }
-  scope :today, -> { where(date: Date.today).order(date: :desc) }
+  scope :today, -> { where(date: Date.today.all_day).order(date: :desc) }
   scope :excluding_date, ->(date) { where.not(date: date) }
 
   def hours = (duration / 3600).to_i
