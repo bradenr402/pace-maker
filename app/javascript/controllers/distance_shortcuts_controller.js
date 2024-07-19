5cc29b6b3c42cd63f0593254786149759d3a533e
Braden <108199884+bradenr402@users.noreply.github.com>
2024-07-19 17:31:00 -0400

    refactor run quick buttons logic

--- app/javascript/controllers/distance_shortcuts_controller.js
+++ app/javascript/controllers/distance_shortcuts_controller.js
@@ -5,32 +5,18 @@ export default class extends Controller {
   static targets = ['distance'];
 
   setDistance(event) {
-    const distance = event.currentTarget.dataset.distanceValue;
+    const distance = event.currentTarget.dataset.setDistanceValue;
 
     this.distanceTarget.value = parseFloat(distance);
   }
 
-  decrementDistance(event) {
-    const decrementValue = event.currentTarget.dataset.decrementValue;
+  updateDistance(event) {
+    const updateDistanceValue = event.currentTarget.dataset.updateDistanceValue;
 
-    if (this.distanceTarget.value === '') {
-      this.distanceTarget.value = 0;
-    }
+    if (this.distanceTarget.value === '') this.distanceTarget.value = 0;
 
     let currentValue = parseFloat(this.distanceTarget.value);
-    currentValue -= parseFloat(decrementValue);
-    this.distanceTarget.value = currentValue;
-  }
-
-  incrementDistance(event) {
-    const incrementValue = event.currentTarget.dataset.incrementValue;
-
-    if (this.distanceTarget.value === '') {
-      this.distanceTarget.value = 0;
-    }
-
-    let currentValue = parseFloat(this.distanceTarget.value);
-    currentValue += parseFloat(incrementValue);
+    currentValue += parseFloat(updateDistanceValue);
     this.distanceTarget.value = currentValue;
   }
 }
