84a642c4292aa09f1f3e72a5139eab6841021b37
Braden <108199884+bradenr402@users.noreply.github.com>
2024-08-09 15:56:11 -0400

    use turbo to fetch teams without full page reload

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -13,6 +13,11 @@ class TeamsController < ApplicationController
       else
         current_user.other_teams
       end
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 
   def show
