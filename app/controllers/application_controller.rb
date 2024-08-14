f67f5efa969183772ede24197501775e5b3b8626
Braden <108199884+bradenr402@users.noreply.github.com>
2024-08-13 21:32:19 -0400

    formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: %i[username email]
     devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
-    devise_parameter_sanitizer.permit :account_update, keys: %i[display_name username email phone_number]
+    devise_parameter_sanitizer.permit :account_update,
+                                      keys: %i[
+                                        display_name
+                                        username
+                                        email
+                                        phone_number
+                                      ]
   end
 end
