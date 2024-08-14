9b633710892a74d6ac0dbed58c39c54a5159cc32
Braden <108199884+bradenr402@users.noreply.github.com>
2024-08-13 21:32:29 -0400

    remove comment

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -81,13 +81,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(
-  #     :account_update,
-  #     keys: %i[display_name username email phone_number gender]
-  #   )
-  # end
-
   def configure_account_update_params
     devise_parameter_sanitizer.permit(
       :account_update,
