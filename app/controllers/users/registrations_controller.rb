f8f1f9ab351a12b0141a27cd87336fc9f17e3cd4
Braden <108199884+bradenr402@users.noreply.github.com>
2024-08-14 17:21:34 -0400

    lint: rubocop unused method argument

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -102,7 +102,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
 
-  def after_update_path_for(resource)
+  def after_update_path_for(_resource)
     edit_user_registration_path
   end
 
