2c5025d637834caeb0af70b52183690e0614d20f
Braden <108199884+bradenr402@users.noreply.github.com>
2024-08-18 16:38:05 -0400

    add user `show_email?` and `show_phone?` helper methods

--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -8,4 +8,8 @@ module UserSettings
       s.key :notifications, defaults: { in_app: true }
     end
   end
+
+  def show_email? = settings(:privacy).email_visible
+
+  def show_phone? = settings(:privacy).phone_visible
 end
