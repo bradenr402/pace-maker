edfe59b75668227d0a99b3451c8121175aa10c50
Braden <108199884+bradenr402@users.noreply.github.com>
2024-08-20 09:50:21 -0400

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'users#profile'
+
   devise_for :users,
              controllers: {
                registrations: 'users/registrations',
@@ -6,34 +8,40 @@ Rails.application.routes.draw do
              }
 
   resources :runs, except: %i[index]
-  resources :users, only: %i[show]
-  resources :teams do
-    resource :team_settings, only: %i[update], path: 'settings', as: :settings
+
+  resources :users, only: %i[show] do
+    resource :user_settings,
+             only: %i[update],
+             path: 'settings',
+             as: :settings,
+             controller: 'user_settings'
   end
-  resources :team_join_requests, only: %i[update]
 
-  post 'remove_member', to: 'teams#remove_member'
+  resources :teams do
+    resource :team_settings,
+             only: %i[update],
+             path: 'settings',
+             as: :settings,
+             controller: 'team_settings'
 
-  patch 'team_join_requests/:id/cancel',
-        to: 'team_join_requests#cancel',
-        as: 'cancel_request'
-  patch 'team_join_requests/:id/approve',
-        to: 'team_join_requests#approve',
-        as: 'approve_request'
-  patch 'team_join_requests/:id/reject',
-        to: 'team_join_requests#reject',
-        as: 'reject_request'
+    member do
+      post :remove_member
+      post :join
+      post :leave
+    end
+  end
 
-  post 'teams/:id/join', to: 'teams#join', as: 'join_team'
-  post 'teams/:id/leave', to: 'teams#leave', as: 'leave_team'
-
-  resource :user_settings, only: %i[update]
+  resources :team_join_requests, only: %i[update] do
+    member do
+      patch :cancel
+      patch :approve
+      patch :reject
+    end
+  end
 
   get 'search', to: 'search#index', as: :search
 
-  root 'users#profile'
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', :as => :rails_health_check
+  get 'up', to: 'rails/health#show', as: :rails_health_check
 end
