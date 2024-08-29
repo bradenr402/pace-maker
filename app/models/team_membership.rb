944e16b6175bfedf69516ac37134185001941994
Braden <108199884+bradenr402@users.noreply.github.com>
2024-08-29 09:57:27 -0400

    redesign _info partials

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -7,4 +7,30 @@ class TeamMembership < ApplicationRecord
               scope: :team_id,
               message: 'is already a member of the team'
             }
+
+  delegate :season_progress, to: :team
+
+  # TODO: remove this later
+  def mileage_goal? = true
+
+  # TODO: remove this later
+  def mileage_goal = 100
+
+  # TODO: implement this
+  def miles_completed = 42
+
+  # TODO: implement this
+  def mileage_goal_progress = 42
+
+  # TODO: implement this
+  def miles_remaining_in_goal = mileage_goal - miles_completed
+
+  # TODO: implement this
+  def meeting_goal? = true
+
+  # TODO: implement this
+  def ahead_of_goal? = false
+
+  # TODO: implement this
+  def behind_goal? = false
 end
