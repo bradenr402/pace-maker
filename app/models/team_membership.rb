bf2255c14f92beaf901c9ab040aa07f2bbb4edc3
Braden <108199884+bradenr402@users.noreply.github.com>
2024-09-01 11:59:25 -0400

    fix: `member` used instead of `user`

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -54,7 +54,7 @@ class TeamMembership < ApplicationRecord
   def mileage_goal_progress_message(current_user)
     possessive = user == current_user ? 'your' : user.gender_possessive
     met_goal = user == current_user ? "you've" : "#{user.first_name} has"
-    season_state = user == current_user ? "You're" : "#{member.first_name} is"
+    season_state = user == current_user ? "You're" : "#{user.first_name} is"
     team_state = user == current_user ? "You're" : 'Your team is'
 
     if mileage_goal_complete?
