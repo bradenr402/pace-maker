4bc3b50491d4cabc3399ec36979153d4429d041e
Braden <108199884+bradenr402@users.noreply.github.com>
2024-09-30 13:04:28 -0400

    fix: convert team settings to integers

--- app/models/team_settings.rb
+++ app/models/team_settings.rb
@@ -27,13 +27,13 @@ module TeamSettings
   end
 
   def require_gender? = settings(:join_requirements).require_gender
-  def max_allowed_requests = settings(:join_requirements).max_allowed_requests
+  def max_allowed_requests = settings(:join_requirements).max_allowed_requests.to_i
 
   def week_start = settings(:general).week_start.to_sym
 
-  def long_run_distance_male = settings(:long_runs).long_run_distance_male
-  def long_run_distance_female = settings(:long_runs).long_run_distance_female
-  def long_run_distance_neutral = settings(:long_runs).long_run_distance_neutral
+  def long_run_distance_male = settings(:long_runs).long_run_distance_male.to_i
+  def long_run_distance_female = settings(:long_runs).long_run_distance_female.to_i
+  def long_run_distance_neutral = settings(:long_runs).long_run_distance_neutral.to_i
 
   def include_saturday_in_streak? = settings(:streaks).include_saturday
   def include_sunday_in_streak? = settings(:streaks).include_sunday
@@ -44,7 +44,7 @@ module TeamSettings
     (date.saturday? && exclude_saturday_from_streak?) ||
       (date.sunday? && exclude_sunday_from_streak?)
 
-  def streak_distance_male = settings(:streaks).streak_distance_male
-  def streak_distance_female = settings(:streaks).streak_distance_female
-  def streak_distance_neutral = settings(:streaks).streak_distance_neutral
+  def streak_distance_male = settings(:streaks).streak_distance_male.to_i
+  def streak_distance_female = settings(:streaks).streak_distance_female.to_i
+  def streak_distance_neutral = settings(:streaks).streak_distance_neutral.to_i
 end
