1d4645a180bdf9895333340145974e9b0515fab1
Braden <108199884+bradenr402@users.noreply.github.com>
2024-10-05 11:28:33 -0400

    fix: remove show calendar button when there is no calendar to show

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -5,15 +5,26 @@ export default class extends Controller {
   static targets = ['toggleButtonText', 'calendarContainer'];
   static classes = ['hidden', 'visible'];
 
+  connect() {
+    if (this.hasToggleButtonTextTarget && this.hasCalendarContainerTarget)
+      this.hideCalendar();
+  }
+
   toggleCalendar() {
-    if (this.calendarContainerTarget.classList.contains(...this.hiddenClasses)) {
-      this.calendarContainerTarget.classList.remove(...this.hiddenClasses);
-      this.calendarContainerTarget.classList.add(...this.visibleClasses);
-      this.toggleButtonTextTarget.textContent = 'Hide calendar';
-    } else {
-      this.calendarContainerTarget.classList.add(...this.hiddenClasses);
-      this.calendarContainerTarget.classList.remove(...this.visibleClasses);
-      this.toggleButtonTextTarget.textContent = 'Show calendar';
-    }
+    if (this.calendarContainerTarget.classList.contains(...this.hiddenClasses))
+      this.showCalendar();
+    else this.hideCalendar();
+  }
+
+  hideCalendar() {
+    this.calendarContainerTarget.classList.add(...this.hiddenClasses);
+    this.calendarContainerTarget.classList.remove(...this.visibleClasses);
+    this.toggleButtonTextTarget.textContent = 'Show calendar';
+  }
+
+  showCalendar() {
+    this.calendarContainerTarget.classList.remove(...this.hiddenClasses);
+    this.calendarContainerTarget.classList.add(...this.visibleClasses);
+    this.toggleButtonTextTarget.textContent = 'Hide calendar';
   }
 }
