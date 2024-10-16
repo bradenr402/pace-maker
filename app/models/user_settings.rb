212911258e3a2f423c448140a8b8b4191259088a
Braden <108199884+bradenr402@users.noreply.github.com>
2024-10-16 14:06:16 -0400

    feat: add `UserSettings#defaults` method for accessing default settings

--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -1,11 +1,26 @@
 module UserSettings
   extend ActiveSupport::Concern
 
+  def self.defaults
+    {
+      privacy: {
+        email_visible: false,
+        phone_visible: false
+      },
+      appearance: {
+        theme: 'system'
+      },
+      notifications: {
+        in_app: true
+      }
+    }
+  end
+
   included do
     has_settings do |s|
-      s.key :privacy, defaults: { email_visible: false, phone_visible: false }
-      s.key :appearance, defaults: { theme: 'system' }
-      s.key :notifications, defaults: { in_app: true }
+      s.key :privacy, defaults: UserSettings.defaults[:privacy]
+      s.key :appearance, defaults: UserSettings.defaults[:appearance]
+      s.key :notifications, defaults: UserSettings.defaults[:notifications]
     end
   end
 
