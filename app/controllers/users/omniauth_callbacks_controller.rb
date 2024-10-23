21a7300556f4f0b62ba8ea52e297bce3ed67e27f
Braden <108199884+bradenr402@users.noreply.github.com>
2024-10-23 08:55:47 -0400

    refactor(omniauth): refactor `google_oauth2` callback
    - Separate flow into separate methods:
      - `handle_existing_account`
      - `handle_signed_in_user`
      - `handle_not_signed_in_user`
      - `link_google_account`
      - `handle_successful_authentication`
      - `handle_failed_authentication`
    - Ensure selected Google account matches the email on the current user's account

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,34 +10,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def twitter
   # end
 
-  def google_oauth2
-    @user = User.from_omniauth(auth)
-
-    if @user.nil? # account already exists with email
-      if user_signed_in?
-        if current_user.update(provider: auth.provider, uid: auth.uid)
-          flash[:success] = 'Your Google account was linked successfully.'
-        else
-          flash[:error] = 'There was an issue linking your Google account.'
-        end
-        redirect_to edit_user_registration_path
-      else # account already exists with email, but user is not signed in
-        flash[:alert] = "An account already exists with the email <b>#{auth.info.email}</b>. Please sign in to link your Google account."
-        store_location_for(:user, edit_user_registration_path)
-        redirect_to new_user_session_path
-      end
-    elsif @user.persisted? # not new_record? (i.e., account created and saved without errors)
-      sign_out_all_scopes
-      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session['devise.google_data'] = request.env['omniauth.auth'].except('extra')
-      flash[:error] =
-        t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
-      redirect_to new_user_session_path
-    end
-  end
-
   # More info at:
   # https://github.com/heartcombo/devise#omniauth
 
@@ -58,7 +30,61 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super(scope)
   # end
 
+  def google_oauth2
+    @user = User.from_omniauth(auth)
+
+    if @user.nil? # account already exists with email
+      handle_existing_account
+    elsif @user.persisted? # account created and saved without errors
+      handle_successful_authentication
+    else # error creating/linking account
+      handle_failed_authentication
+    end
+  end
+
   private
+
+  def handle_existing_account
+    user_signed_in? ? handle_signed_in_user : handle_not_signed_in_user
+  end
+
+  def handle_signed_in_user
+    if current_user.email == auth.info.email
+      link_google_account
+    else
+      flash[
+        :error
+      ] = 'The email on your Google account does not match the one on your account.'
+    end
+  end
+
+  def handle_not_signed_in_user
+    store_location_for(:user, edit_user_registration_path)
+    redirect_to new_user_session_path, alert: "An account already exists with the email <b>#{auth.info.email}</b>. Please sign in to link your Google account."
+  end
+
+  def link_google_account
+    if current_user.update(provider: auth.provider, uid: auth.uid)
+      flash[:success] = 'Your Google account was linked successfully.'
+    else
+      flash[:error] = 'There was an issue linking your Google account.'
+    end
+    redirect_to edit_user_registration_path
+  end
+
+  def handle_successful_authentication
+    sign_out_all_scopes
+    flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+    sign_in_and_redirect @user, event: :authentication
+  end
+
+  def handle_failed_authentication
+    session['devise.google_data'] = request.env['omniauth.auth'].except('extra')
+    flash[:error] = t 'devise.omniauth_callbacks.failure',
+      kind: 'Google',
+      reason: "#{auth.info.email} is not authorized."
+    redirect_to new_user_session_path
+  end
 
   def auth
     @auth ||= request.env['omniauth.auth']
