6c6584d53702e252563c72b08553905723b4ccae
bradenr402 <bradenr402@gmail.com>
2024-11-07 14:53:23 -0500

    fix(calendar): calendar could not be toggle if season was over

--- app/models/team.rb
+++ app/models/team.rb
@@ -45,8 +45,7 @@ class Team < ApplicationRecord
   # Methods
   def season_dates? = season_start_date.present? && season_end_date.present?
 
-  def season_in_progress? =
-    season_dates? && Date.current.between?(season_start_date, season_end_date)
+  def season_in_progress? = season_dates? && season_start_date.past? && season_end_date.future?
 
   def season_over? = season_dates? && season_end_date.past?
 
