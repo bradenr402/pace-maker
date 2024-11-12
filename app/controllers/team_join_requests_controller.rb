ad9913544851f3707d5bf4cb99cf5ebe4e78987b
bradenr402 <bradenr402@gmail.com>
2024-11-12 09:47:11 -0500

    refactor(TeamJoinRequestsController): simplify `before_action` callbacks

--- app/controllers/team_join_requests_controller.rb
+++ app/controllers/team_join_requests_controller.rb
@@ -1,7 +1,6 @@
 class TeamJoinRequestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_join_request, only: %i[update cancel approve reject]
-  before_action :set_team, only: %i[cancel approve reject]
+  before_action :set_join_request, :set_team, only: %i[update cancel approve reject]
   before_action :authorize_owner!, only: %i[approve reject]
 
   def update
