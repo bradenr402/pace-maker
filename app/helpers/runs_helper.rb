24570198d13a8d3d5fd1a695a11d3519db26ed8d
bradenr402 <bradenr402@gmail.com>
2024-11-14 10:12:01 -0500

    refactor(runs): add team id as url param to keep run badges on runs#show

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -1,11 +1,10 @@
 module RunsHelper
   def run_content_wrapper(run, team, turbo_frame, &content)
-    if current_page?(run) || turbo_frame
-      capture(&content)
-    else
-      link_to run, data: { turbo: false }, run:, team:, class: 'block space-y-4' do
-        capture(&content)
-      end
+    content = capture(&content)
+    return content if current_page?(run) || turbo_frame
+
+    link_to run_path(run, team: team), team:, class: 'block space-y-4', data: { turbo: false } do
+      content
     end
   end
 end
