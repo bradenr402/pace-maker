965e9d1c385d11107b64cf58d0bde9f79a1953c4
bradenr402 <bradenr402@gmail.com>
2024-11-15 20:02:37 -0500

    feat(PWA): make app PWA capable

--- config/routes.rb
+++ config/routes.rb
@@ -68,6 +68,8 @@ Rails.application.routes.draw do
 
   resources :feedback_form, only: %i[create]
 
+  get 'service-worker.js', to: 'service_worker#service_worker'
+  get 'manifest.json', to: 'service_worker#manifest'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up', to: 'rails/health#show', as: :rails_health_check
