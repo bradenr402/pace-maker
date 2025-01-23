256450713bd858e2fbe5dbb4d212c4a2af3a1925
bradenr402 <bradenr402@gmail.com>
2025-01-23 10:33:19 -0500

    fix: require gender checkbox could still be toggle even when disabled

--- app/javascript/controllers/toggle_checkbox_controller.js
+++ app/javascript/controllers/toggle_checkbox_controller.js
@@ -1,11 +1,11 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="toggle-checkbox"
 export default class extends Controller {
-  static targets = ['checkbox']
+  static targets = ['checkbox'];
 
   toggle() {
-    this.checkboxTarget.checked = !this.checkboxTarget.checked;
+    if (!this.checkboxTarget.disabled) this.checkboxTarget.checked = !this.checkboxTarget.checked;
     this.checkboxTarget.dispatchEvent(new Event('change'));
   }
 }
