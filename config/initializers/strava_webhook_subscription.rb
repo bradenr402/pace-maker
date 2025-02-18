da916421fd240e670048bba7d7f1b2e657756ec5
bradenr402 <bradenr402@gmail.com>
2025-02-17 22:17:18 -0500

    fix: fix webhook initializer to avoid missing table error

--- config/initializers/strava_webhook_subscription.rb
+++ config/initializers/strava_webhook_subscription.rb
@@ -1,3 +1,7 @@
 Rails.application.config.after_initialize do
-  StravaService.create_global_webhook_subscription if Rails.env.production?
+  if ActiveRecord::Base.connection.table_exists?('strava_webhook_subscriptions')
+    StravaService.create_global_webhook_subscription
+  else
+    Rails.logger.warn('Strava webhook subscription skipped: table does not exist.')
+  end
 end
