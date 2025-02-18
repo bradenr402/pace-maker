8e7da59a68aebb372832d4393f243806e49d2e88
bradenr402 <bradenr402@gmail.com>
2025-02-17 22:51:59 -0500

    fix: remove undefined `subscribe_to_webhook` method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -123,8 +123,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
       settings_params.slice(:auto_import_strava)
     )
 
-    StravaService.subscribe_to_webhook(resource) if settings_params[:auto_import_strava]
-
     if ((resource.google_account_linked? || resource.strava_account_linked?) && resource.password_changed_at.nil?) || params['password'].blank?
       resource.update_without_password(params.except(:current_password))
     else
