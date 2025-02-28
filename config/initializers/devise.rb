222a91568711720ea6ca7a87ada030d442e6a7c5
Braden Roth <bradenr402@gmail.com>
2025-02-28 12:27:43 -0500

    fix(strava): restore client secret and approval prompt in `config.omniauth`

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -281,8 +281,10 @@ Devise.setup do |config|
 
   config.omniauth :strava,
                   Rails.application.credentials[:strava_client_id],
-                  redirect_uri: 'https://localhost:300/users/auth/strava/callback',
+                  Rails.application.credentials[:strava_client_secret],
+                  redirect_uri: 'https://pace-maker.onrender.com/users/auth/strava/callback',
                   response_type: 'code',
+                  approval_prompt: 'force',
                   scope: 'read,activity:read,profile:read_all'
 
   # ==> Warden configuration
