f2201a5d34dbabac6562d752f635e80a2bb08e5f
Braden Roth <bradenr402@gmail.com>
2025-03-15 17:47:57 -0400

    feat(policies): add breadcrumbs to policy pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,20 @@
 class PagesController < ApplicationController
   layout 'landing', only: [:landing]
 
-  def privacy_policy; end
-  def strava_policy; end
-  def google_policy; end
+  def privacy_policy
+    add_breadcrumb 'Settings', edit_user_registration_path
+    add_breadcrumb 'Help', edit_user_registration_path(tab: 'helpTab')
+    add_breadcrumb 'Privacy Policy', privacy_policy_path
+  end
+  def strava_policy
+    add_breadcrumb 'Settings', edit_user_registration_path
+    add_breadcrumb 'Help', edit_user_registration_path(tab: 'helpTab')
+    add_breadcrumb 'Strava Data Usage Policy', strava_policy_path
+  end
+  def google_policy
+    add_breadcrumb 'Settings', edit_user_registration_path
+    add_breadcrumb 'Help', edit_user_registration_path(tab: 'helpTab')
+    add_breadcrumb 'Google Data Usage Policy', google_policy_path
+  end
   def landing; end
 end
