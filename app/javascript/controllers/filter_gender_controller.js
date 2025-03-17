31b05d844da80323af7533429b57e6d03e5aed4f
Braden Roth <bradenr402@gmail.com>
2025-03-17 11:23:05 -0400

    style(rankings): redesign gender filters on team rankings as radio chips

--- app/javascript/controllers/filter_gender_controller.js
+++ app/javascript/controllers/filter_gender_controller.js
@@ -3,35 +3,33 @@ import { Controller } from '@hotwired/stimulus';
 // Connects to data-controller="filter-gender"
 export default class extends Controller {
   static targets = [
-    'filter',
+    'radio',
     'genderCell',
     'row',
     'maleTotal',
     'femaleTotal',
+    'unspecifiedTotal',
     'rankingsTable',
   ];
 
-  filter() {
-    const selectedGender = this.filterTarget.value;
+  getValue() {
+    const selectedRadio = this.radioTargets.find((radio) => radio.checked);
+    return selectedRadio ? selectedRadio.value : null;
+  }
 
-    if (selectedGender === 'all') {
-      this.maleTotalTarget.classList.remove('hidden');
-      this.femaleTotalTarget.classList.remove('hidden');
-    } else if (selectedGender === 'male') {
-      this.femaleTotalTarget.classList.add('hidden');
-      this.maleTotalTarget.classList.remove('hidden');
-    } else if (selectedGender === 'female') {
-      this.femaleTotalTarget.classList.remove('hidden');
-      this.maleTotalTarget.classList.add('hidden');
-    }
+  filter() {
+    const selectedGender = this.getValue();
+
+    if (selectedGender === 'all') this.showAllTotals();
+    else if (selectedGender === 'male') this.showMaleTotals();
+    else if (selectedGender === 'female') this.showFemaleTotals();
+    else if (selectedGender == 'unspecified') this.showUnspecifiedTotals();
 
     this.rowTargets.forEach((row) => {
-      const genderCell = row.querySelector(
-        "[data-filter-gender-target='genderCell']"
-      );
+      const genderCell = row.querySelector("[data-filter-gender-target='genderCell']");
 
       if (genderCell) {
-        const gender = genderCell.textContent.trim();
+        const gender = genderCell.dataset.gender;
         if (selectedGender === 'all' || gender.toLowerCase() === selectedGender)
           row.classList.remove('hidden');
         else row.classList.add('hidden');
@@ -39,7 +37,31 @@ export default class extends Controller {
     });
   }
 
+  showAllTotals() {
+    if (this.hasMaleTotalTarget) this.maleTotalTarget.classList.remove('hidden');
+    if (this.hasFemaleTotalTarget) this.femaleTotalTarget.classList.remove('hidden');
+    if (this.hasUnspecifiedTotalTarget) this.unspecifiedTotalTarget.classList.remove('hidden');
+  }
+
+  showMaleTotals() {
+    if (this.hasMaleTotalTarget) this.maleTotalTarget.classList.remove('hidden');
+    if (this.hasFemaleTotalTarget) this.femaleTotalTarget.classList.add('hidden');
+    if (this.hasUnspecifiedTotalTarget) this.unspecifiedTotalTarget.classList.add('hidden');
+  }
+
+  showFemaleTotals() {
+    if (this.hasFemaleTotalTarget) this.femaleTotalTarget.classList.remove('hidden');
+    if (this.hasMaleTotalTarget) this.maleTotalTarget.classList.add('hidden');
+    if (this.hasUnspecifiedTotalTarget) this.unspecifiedTotalTarget.classList.add('hidden');
+  }
+
+  showUnspecifiedTotals() {
+    if (this.hasUnspecifiedTotalTarget) this.unspecifiedTotalTarget.classList.remove('hidden');
+    if (this.hasMaleTotalTarget) this.maleTotalTarget.classList.add('hidden');
+    if (this.hasFemaleTotalTarget) this.femaleTotalTarget.classList.add('hidden');
+  }
+
   rankingsTableTargetConnected() {
-    if (this.hasFilterTarget) this.filter();
+    if (this.hasRadioTarget) this.filter();
   }
 }
