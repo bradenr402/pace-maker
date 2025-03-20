839b6a76e1415394b4bde8ea7903a21c45d80e47
Braden Roth <bradenr402@gmail.com>
2025-03-19 23:52:14 -0400

    style(home): add activity dot to tabs, and improve UI of team audits

--- app/models/team_audit.rb
+++ app/models/team_audit.rb
@@ -6,4 +6,19 @@ class TeamAudit < ApplicationRecord
 
     key.to_sym
   end
+
+  def attr_removed? = old_value.present? && new_value.blank?
+
+  def attr_added? = old_value.blank? && new_value.present?
+
+  alias attribute_added? attr_added?
+  alias attribute_removed? attr_removed?
+
+  def attr_is_date? = attribute_name == 'season_start_date' || attribute_name == 'season_end_date'
+
+  def attr_is_description? = attribute_name == 'description'
+
+  def attr_is_mileage_goal? = attribute_name == 'mileage_goal'
+
+  def attr_is_long_run_goal? = attribute_name == 'long_run_goal'
 end
