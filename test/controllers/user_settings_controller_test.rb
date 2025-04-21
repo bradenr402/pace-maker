5b1fcc4c0322fafaa8c0352629492370d0dc247b
Braden Roth <bradenr402@gmail.com>
2025-04-21 14:05:05 -0400

    test(controllers): remove unimplemented default controller tests

--- test/controllers/user_settings_controller_test.rb
+++ test/controllers/user_settings_controller_test.rb
@@ -1,13 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class UserSettingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get user_settings_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get user_settings_update_url
-    assert_response :success
-  end
 end
