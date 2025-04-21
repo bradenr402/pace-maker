16c89135f390d7b406845427e32aaa5270723694
Braden Roth <bradenr402@gmail.com>
2025-04-21 12:52:31 -0400

    test(join_requests): add tests for TeamJoinRequest model

--- test/models/team_join_request_test.rb
+++ test/models/team_join_request_test.rb
@@ -1,7 +1,51 @@
-require "test_helper"
+require 'test_helper'
 
 class TeamJoinRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:two)
+    @team = teams(:one)
+    @request = TeamJoinRequest.new(user: @user, team: @team)
+  end
+
+  test 'should be valid with valid attributes' do
+    assert @request.valid?
+  end
+
+  test 'should not allow duplicate join requests for the same team' do
+    @request.save!
+    duplicate = TeamJoinRequest.new(user: @user, team: @team)
+    assert_not duplicate.valid?
+    assert_includes duplicate.errors[:user_id], 'has already sent a join request'
+  end
+
+  test 'should be blocked if rejected and request number exceeds max allowed' do
+    @request.rejected!
+    @request.request_number = @team.max_allowed_requests
+    assert @request.blocked?
+  end
+
+  test 'should not be allowed if blocked' do
+    @request.request_number = @team.max_allowed_requests
+    assert_not @request.allowed?
+  end
+
+  test 'should not be allowed if user is already pending' do
+    @request.status = :pending
+    assert_not @request.allowed?
+  end
+
+  test 'should not be allowed if user is already approved' do
+    @request.approved!
+    assert_not @request.allowed?
+  end
+
+  test 'should not be allowed if user is already a member' do
+    @team.team_memberships.build(user: @user)
+    assert_not @request.allowed?
+  end
+
+  test 'should not be allowed if user owns the team' do
+    @team.owner = @user
+    assert_not @request.allowed?
+  end
 end
