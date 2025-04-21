3bc4fabc437410293b96f5ec9bde4a5b4c96d09f
Braden Roth <bradenr402@gmail.com>
2025-04-21 13:33:01 -0400

    test(user_topics): add tests for UserTopic model

--- test/models/user_topic_test.rb
+++ test/models/user_topic_test.rb
@@ -1,7 +1,30 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTopicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @topic = topics(:one)
+    @user_topic = user_topics(:one)
+  end
+
+  test 'should be valid with valid attributes' do
+    assert @user_topic.valid?
+  end
+
+  test 'should not allow duplicate user-topic pair' do
+    duplicate = UserTopic.new(user: @user, topic: @topic)
+    assert_not duplicate.valid?
+    assert_includes duplicate.errors[:user_id], 'has already been taken'
+  end
+
+  test 'should mark as favorited' do
+    @user_topic.favorite!
+    assert @user_topic.favorited
+  end
+
+  test 'should unfavorite' do
+    @user_topic.update!(favorited: true)
+    @user_topic.unfavorite!
+    assert_not @user_topic.favorited
+  end
 end
