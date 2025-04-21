757b27c096d6bb7b0221702db25c2c8fd6a4bd06
Braden Roth <bradenr402@gmail.com>
2025-04-21 13:00:41 -0400

    test(team_memberships): add tests for TeamMembership model

--- test/models/team_membership_test.rb
+++ test/models/team_membership_test.rb
@@ -1,7 +1,34 @@
-require "test_helper"
+require 'test_helper'
 
 class TeamMembershipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:two)
+    @team = teams(:one)
+    @existing_membership = team_memberships(:one)
+  end
+
+  test 'valid team membership' do
+    membership = TeamMembership.new(user: @user, team: @team)
+
+    assert membership.valid?
+  end
+
+  test 'invalid without user' do
+    membership = TeamMembership.new(team: @team)
+
+    assert_not membership.valid?
+    assert_includes membership.errors[:user], 'must exist'
+  end
+
+  test 'invalid without team' do
+    membership = TeamMembership.new(user: @user)
+
+    assert_not membership.valid?
+  end
+
+  test 'enforces uniqueness of user scoped to team' do
+    duplicate = TeamMembership.new(user: @existing_membership.user, team: @existing_membership.team)
+
+    assert_not duplicate.valid?
+  end
 end
