518c4a25c1aa9c8c73dd05780f18f490b26f9753
Braden Roth <bradenr402@gmail.com>
2025-04-25 23:40:02 -0400

    style(settings): improve wording for clarity and better UX

--- app/javascript/controllers/form_help_text_controller.js
+++ app/javascript/controllers/form_help_text_controller.js
@@ -3,6 +3,7 @@ import { Controller } from '@hotwired/stimulus';
 // Connects to data-controller="form-help-text"
 export default class extends Controller {
   static targets = ['input', 'output', 'plural'];
+  static values = { showSingle: { type: Boolean, default: false } };
 
   update() {
     let inputValue = Math.round(this.inputTarget.value);
@@ -12,7 +13,8 @@ export default class extends Controller {
 
       if (inputValue == 1) {
         this.pluralTarget.classList.add('hidden');
-        this.outputTarget.classList.add('hidden');
+        this.showSingleValue ? this.outputTarget.classList.remove('hidden') : this.outputTarget.classList.add('hidden');
+
       } else {
         this.pluralTarget.classList.remove('hidden');
         this.outputTarget.classList.remove('hidden');
